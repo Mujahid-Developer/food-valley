98e36843dc2d2572acb1bd2c46d771b098a892a9
Mujahid-Developer <mahfuzshikder560@gmail.com>
2021-08-30 01:29:54 +0600

    adding api

---
+++ js/app.js
@@ -0,0 +1,28 @@
+const inputValue = document.getElementById('inputValue').value;
+const loadData = () => {
+    console.log(inputValue)
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`)
+        .then(response => response.json())
+        .then(data => showSearchResults(data.meals))
+}
+// inputValue.value = '';
+
+const showSearchResults = (meals) => {
+    const cardContainer = document.getElementById('card-addon');
+    meals.forEach(item => {
+        console.log(item)
+        const div = document.createElement('div');
+        div.classList.add('col')
+        div.innerHTML = "";
+        div.innerHTML = `
+        <div class="card">
+          <img src="${item.strMealThumb}" class="card-img-top w-100" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${item.strMeal}</h5>
+            <p class="card-text">${item.strInstructions.slice(0, 200)}</p>
+          </div>
+        </div>`
+        cardContainer.appendChild(div);
+    })
+
+}
