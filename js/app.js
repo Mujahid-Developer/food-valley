a42621ba6f773dd05c655ca640c04b9ad57a3072
Mujahid-Developer <mujahidulislam575@gmail.com>
2021-09-03 02:08:32 +0600

    Add Spinner & Delete Previous data when new search

--- js/app.js
+++ js/app.js
@@ -1,26 +1,30 @@
-const inputValue = document.getElementById('inputValue').value;
+const spinner = document.getElementById("spinner");
 const loadData = () => {
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`)
-        .then(response => response.json())
-        .then(data => showSearchResults(data.meals))
-}
+  const inputValue = document.getElementById("inputValue").value;
+  spinner.removeAttribute("hidden");
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`)
+    .then((response) => response.json())
+    .then((data) => {
+      spinner.setAttribute("hidden", "");
+      showSearchResults(data.meals);
+    });
+  inputValue.value = " ";
+};
 
+const cardContainer = document.getElementById("card-addon");
+const div = document.createElement("div");
+div.classList.add("col");
 const showSearchResults = (meals) => {
-    const cardContainer = document.getElementById('card-addon');
-    meals.forEach(item => {
-        const div = document.createElement('div');
-        div.classList.add('col')
-        div.innerHTML = " ";
-        div.innerHTML = `
+  div.innerHTML = " ";
+  meals.forEach((item) => {
+    div.innerHTML = `
         <div class="card">
           <img src="${item.strMealThumb}" class="card-img-top w-100" alt="...">
           <div class="card-body">
             <h5 class="card-title">${item.strMeal}</h5>
             <p class="card-text">${item.strInstructions.slice(0, 200)}</p>
           </div>
-        </div>`
-        cardContainer.appendChild(div);
-        inputValue.value = " ";
-    })
-
-}
+        </div>`;
+    cardContainer.appendChild(div);
+  });
+};
